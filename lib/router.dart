f67255ace5b76b0322e2eaeba0d7812f1ec1fc39
yyupyong <yupyong.kim@gmail.com>
2023-07-06 19:22:30 +0900

    routerクラスを変更

---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:grid_sample/screens/detailsScreen.dart';
+import 'package:grid_sample/screens/homeScreen.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const HomeScreen();
+      },
+    ),
+    GoRoute(
+      path: "/details",
+      builder: (context, state) {
+        return const DetailsScreen();
+      },
+    )
+  ],
+);
