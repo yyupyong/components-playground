023ee1707193073ddc6de67ad2be06c36569b1c6
yyupyong <yupyong.kim@gmail.com>
2023-07-06 19:22:41 +0900

    mainを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:grid_sample/tinder_swipe.dart';
-import 'package:grid_sample/tuturial_screen.dart';
-
-import 'grid_view.dart';
+import 'package:grid_sample/router.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,16 +10,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
-          child: Center(
-            child: TiderSwipe(),
-          ),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
