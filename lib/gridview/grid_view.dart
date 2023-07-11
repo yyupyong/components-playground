1893ef708aaead7ebf60e8aab7ac0b6ebbda04f6
yyupyong <yupyong.kim@gmail.com>
2023-07-11 10:46:50 +0900

    一旦fix

--- lib/gridview/grid_view.dart
+++ lib/gridview/grid_view.dart
@@ -7,18 +7,21 @@ class GridContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-          width: 164.5,
-          height: 228,
+          width: 180.5,
+          height: 250,
           decoration: BoxDecoration(
               color: Colors.red, borderRadius: BorderRadius.circular(10)),
         ),
-        SizedBox(height: 5),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Text('24歳 東京都'), SizedBox(height: 5), Text('自己紹介')],
+        const SizedBox(height: 15),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Column(
+              children: [Text('24歳 東京都'), SizedBox(height: 5), Text('自己紹介')],
+            ),
+          ],
         )
       ],
     );
@@ -26,26 +29,22 @@ class GridContainer extends StatelessWidget {
 }
 
 Widget createGridView() {
-  return Padding(
-    padding: EdgeInsets.symmetric(horizontal: 12),
-    child: GridView.builder(
-      itemCount: 10,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        childAspectRatio: 164.5 / 274,
-        crossAxisCount: 2,
-      ),
-      itemBuilder: (context, index) {
-        return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => PageViewcreater(
-                            index: index,
-                          )));
-            },
-            child: const GridContainer());
-      },
-    ),
+  return GridView.builder(
+    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+    itemCount: 10,
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        childAspectRatio: 0.6, crossAxisCount: 2, mainAxisSpacing: 10),
+    itemBuilder: (context, index) {
+      return GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PageViewcreater(
+                          index: index,
+                        )));
+          },
+          child: const GridContainer());
+    },
   );
 }
