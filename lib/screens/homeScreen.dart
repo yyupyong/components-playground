83a53c9d5c9e4d8194d8896a8235aa8724b71bc2
yyupyong <yupyong.kim@gmail.com>
2023-07-12 20:01:33 +0900

    fix

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -9,7 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: createGridView(),
+      body: GridV(),
     );
   }
 }
